08d2d49b65bafe52b154fd8f295f07464b0827bc
Joseph Filbert <josephfilbert97@gmail.com>
2018-07-01 20:28:10 +0700

    Create OrderByExpressionSqlTranslatorTest

---
+++ MyLinqTranslatorUtilities.Tests/OrderByExpressionSqlTranslatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Xunit;
+
+namespace MyLinqTranslatorUtilities.Tests
+{
+    public class OrderByExpressionSqlTranslatorTests
+    {
+        [Fact]
+        public void OrderBySingleMemberTest()
+        {
+            Expression<Func<IQueryable<SharedClassStructures.Person>, IOrderedQueryable<SharedClassStructures.Person>>>
+                sortExpression = p => p.OrderBy(p1 => p1.Name);
+            var expected = "ORDER BY" + Environment.NewLine + "Name ASC" + Environment.NewLine;
+            var actual = new OrderByExpressionSqlTranslator<SharedClassStructures.Person>(sortExpression).GetOrderBySqlStatement();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void OrderByTwoMembersTest()
+        {
+            Expression<Func<IQueryable<SharedClassStructures.Person>, IOrderedQueryable<SharedClassStructures.Person>>>
+                sortExpression = p => p.OrderBy(p1 => p1.Name).ThenByDescending(p1 => p1.Age);
+            var expected = "ORDER BY" + Environment.NewLine + "Name ASC," + Environment.NewLine + "Age DESC" + Environment.NewLine;
+            var actual = new OrderByExpressionSqlTranslator<SharedClassStructures.Person>(sortExpression).GetOrderBySqlStatement();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
