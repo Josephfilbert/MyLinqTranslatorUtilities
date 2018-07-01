f57a0af3e7c7a6ac1d38de4984e825365073f283
Joseph Filbert <josephfilbert97@gmail.com>
2018-07-01 19:30:41 +0700

    Create ExpressionUtilities

---
+++ MyLinqTranslatorUtilities/ExpressionUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyLinqTranslatorUtilities
+{
+    public static class ExpressionUtilities
+    {
+        /// <summary>
+        /// Returns actual <see cref="Expression"/> from quoted Expression Tree
+        /// </summary>
+        /// <param name="expression">The <see cref="Expression"/> which its <see cref="ExpressionType"/> is <see cref="ExpressionType.Quote"/></param>
+        /// <returns>An actual quoted <see cref="Expression"/></returns>
+        public static Expression StripQuotes(this Expression expression)
+        {
+            var current = expression;
+            while (current.NodeType == ExpressionType.Quote)
+            {
+                current = ((UnaryExpression) current).Operand;
+            }
+
+            return current;
+        }
+    }
+}
